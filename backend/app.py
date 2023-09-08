4f54ad3b5e872ed40286ad2d816796974ce3de94
Smit Gabani <gabanismit11@gmail.com>
2023-09-08 01:29:41 -0400

    add messagegroup route and service

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+from services.message_groups import *
+
+
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL') 
 backend = os.getenv('BACKEND_URL')
@@ -23,6 +26,16 @@ def health_check():
         'user_handle': user_handle,
     }
     return jsonify(success)
+
+@app.route("/api/message_groups", methods=['GET'])
+def data_message_groups():
+  user_handle  = 'smitgabani'
+  model = MessageGroups.run(user_handle=user_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
