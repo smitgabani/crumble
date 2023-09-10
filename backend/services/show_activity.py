8bc92b0a1539c1e780ec8b25e678ebbe25ebaa41
Smit Gabani <gabanismit11@gmail.com>
2023-09-10 15:04:10 -0400

    dockerignoer readme and minor

--- backend/services/show_activity.py
+++ backend/services/show_activity.py
@@ -4,7 +4,7 @@ class ShowActivitiy:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
+      'handle':  'Smit Gabani',
       'message': 'Cloud is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
